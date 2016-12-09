59560693aa7d4e7c501db586798d19f78c514604
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2016-12-10 00:54:01 +0200

    menu ch

--- js/misters.js
+++ js/misters.js
@@ -18,82 +18,57 @@ $(function(){
     {name:'fel'},
     {name:'faks'}
     ],
-    text : [
-    {name: `Lorem ipsum dolor sit amet, consectetur 
-        adipisicing elit. Neque animi deleniti error 
-        repellendus, iure totam! Numquam atque commodi 
-        excepturi laborum explicabo, rerum dolorum veritatis
-        odio, velit consectetur, magnam autem necessitatibus.`},
-        {name:`Lorem ipsum dolor sit amet, consectetur 
-            adipisicing elit. Neque animi deleniti error 
-            repellendus, iure totam! Numquam atque commodi 
-            excepturi laborum explicabo, rerum dolorum veritatis
-            odio, velit consectetur, magnam autem necessitatibus.`},
-            {name:`Lorem ipsum dolor sit amet, consectetur 
-                adipisicing elit. Neque animi deleniti error 
-                repellendus, iure totam! Numquam atque commodi 
-                excepturi laborum explicabo, rerum dolorum veritatis
-                odio, velit consectetur, magnam autem necessitatibus.`}
-                ],
-                photo1 : [
-                {name: "mister1_1.png"},
-                {name: "mister1_1.png"},
-                {name: "mister1_1.png"}
-                ],
-                photo2 : [
-                {name: "mister1_2.png"},
-                {name: "mister1_2.png"},
-                {name: "mister1_2.png"}
-                ],
-                vk : [
-                {name: 'https://vk.com/id18213604'},
-                {name: 'https://vk.com/hitodz'},
-                {name: 'https://vk.com/dan1k_1'}
-                ],
-                fb : [
-                {name: '#'},
-                {name: '#'},
-                {name: '#'}
-                ],
-                inst : [
-                {name: '#'},
-                {name: '#'},
-                {name: '#'}
-                ]
-            };
+    photo1 : [
+    {name: "mister1_1.png"},
+    {name: "mister1_1.png"},
+    {name: "mister1_1.png"}
+    ],
+    photo2 : [
+    {name: "mister1_2.png"},
+    {name: "mister1_2.png"},
+    {name: "mister1_2.png"}
+    ],
+    vk : [
+    {name: 'https://vk.com/id18213604'},
+    {name: 'https://vk.com/hitodz'},
+    {name: 'https://vk.com/dan1k_1'}
+    ],
+    fb : [
+    {name: '#'},
+    {name: '#'},
+    {name: '#'}
+    ],
+    inst : [
+    {name: '#'},
+    {name: '#'},
+    {name: '#'}
+    ]
+};
 
-            var content = tmpl(html, data);
+var content = tmpl(html, data);
 
-            $('body').append(content);
+$('body').append(content);
 
-            var overlay = $(".overlay");
-            var modal = $(".modal");
+var overlay = $(".overlay");
+var modal = $(".modal");
 
-            $(".election").on("click", function() {
-                overlay.addClass('overlay_show');
-                modal.addClass('modal_show');
-            });
+$(".election").on("click", function() {
+    overlay.addClass('overlay_show');
+    modal.addClass('modal_show');
+});
 
-            overlay.on("click", function () {
-                $(this).removeClass('overlay_show');
-                modal.removeClass('modal_show');
-            });
+overlay.on("click", function () {
+    $(this).removeClass('overlay_show');
+    modal.removeClass('modal_show');
+});
 
-            $('.show_line,.menu').click(function(){
-                var hidden = $('.menu');
-                if (hidden.hasClass('visible')){
-                    hidden.animate({"top":"-1000px"}, "slow").removeClass('visible');
-                } else {
-                    hidden.animate({"top":"0px"}, "slow").addClass('visible');
-                }
-            });
-            $(document).bind( 'mousewheel', function (e) { 
-                var nt = $(document.body).scrollTop()-(e.originalEvent.wheelDeltaY*3.3); 
-                e.preventDefault(); 
+$('.show_line,.menu').click(function(){
+    var hidden = $('.menu');
+    if (hidden.hasClass('visible')){
+        hidden.animate({"top":"-1000px"}, "slow").removeClass('visible');
+    } else {
+        hidden.animate({"top":"0px"}, "slow").addClass('visible');
+    }
+});
 
-                $(document.body).stop().animate( { 
-                    scrollTop : nt 
-                } , 1000); 
-            } );
-
-        });
+});
